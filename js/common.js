a7e9519109c4f41e07ee1f62c4b8a81c9781cfe4
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-12-24 16:54:43 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.release-wrap').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    arrows: true
+ 
+  });
+  $('.blog-wrap-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    arrows: true
+ 
+  });
+});
